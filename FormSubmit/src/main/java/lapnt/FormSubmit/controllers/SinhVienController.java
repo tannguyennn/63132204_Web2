030a8a30852fce8a9df793ec63209a511276bed8
NguyenTanLap <135099342+tannguyennn@users.noreply.github.com>
2024-03-12 17:06:17 +0700

    upload

---
+++ FormSubmit/src/main/java/lapnt/FormSubmit/controllers/SinhVienController.java
@@ -0,0 +1,23 @@
+package lapnt.FormSubmit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ch.qos.logback.core.model.Model;
+import lapnt.FormSubmit.models.DTOSinhVien;
+
+@Controller
+public class SinhVienController {
+	@GetMapping("/themMoiSV")
+	public String showForm(Model mm) {
+        DTOSinhVien sinhvienNull = new DTOSinhVien();
+        mm.addAttribute("svDTO", sinhvienNull);
+        return "themSV_form";
+    }
+	@PostMapping("/themMoiSV")
+	public String submitForm(@ModelAttribute("svDTO") DTOSinhVien sv) {
+	    return "themSV_OK";
+	}
+}
---
+++ FormSubmit/src/main/java/lapnt/FormSubmit/models/DTOSinhVien.java
@@ -0,0 +1,27 @@
+package lapnt.FormSubmit.models;
+
+public class DTOSinhVien {
+	private String maSoSV;      // Mã số sinh viên
+	private String hoVaTen;     // Họ và Tên
+	public String getMaSoSV() {
+		return maSoSV;
+	}
+	public void setMaSoSV(String maSoSV) {
+		this.maSoSV = maSoSV;
+	}
+	public String getHoVaTen() {
+		return hoVaTen;
+	}
+	public void setHoVaTen(String hoVaTen) {
+		this.hoVaTen = hoVaTen;
+	}
+	public DTOSinhVien(String maSoSV, String hoVaTen) {
+		this.maSoSV = maSoSV;
+		this.hoVaTen = hoVaTen;
+	}
+
+	public DTOSinhVien() {
+		super();
+	}
+	
+}
