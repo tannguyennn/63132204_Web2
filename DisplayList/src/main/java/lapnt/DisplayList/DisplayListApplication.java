5cbb8dfbf583245603b843bcee0fdc5acc46dc83
NguyenTanLap <135099342+tannguyennn@users.noreply.github.com>
2024-03-25 19:48:45 +0700

    upload

---
+++ DisplayList/src/main/java/lapnt/DisplayList/DisplayListApplication.java
@@ -0,0 +1,13 @@
+package lapnt.DisplayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplayListApplication.class, args);
+	}
+
+}
---
+++ DisplayList/src/test/java/lapnt/DisplayList/DisplayListApplicationTests.java
@@ -0,0 +1,13 @@
+package lapnt.DisplayList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
