32540d216182ec5344785d6e84682d102fe9d599
NguyenTanLap <135099342+tannguyennn@users.noreply.github.com>
2024-03-26 14:59:55 +0700

    upload lan 1

---
+++ QuanLySinhVien/src/test/java/lapnt/QuanLySinhVien/QuanLySinhVienApplicationTests.java
@@ -0,0 +1,13 @@
+package lapnt.QuanLySinhVien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLySinhVienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
