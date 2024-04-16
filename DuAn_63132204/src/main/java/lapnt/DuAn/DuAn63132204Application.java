a74076294087d35ba575f0828724fe4e73ea52cc
NguyenTanLap <135099342+tannguyennn@users.noreply.github.com>
2024-04-16 15:25:43 +0700

    upload

---
+++ DuAn_63132204/src/main/java/lapnt/DuAn/DuAn63132204Application.java
@@ -0,0 +1,13 @@
+package lapnt.DuAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAn63132204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAn63132204Application.class, args);
+	}
+
+}
---
+++ DuAn_63132204/src/test/java/lapnt/DuAn/DuAn63132204ApplicationTests.java
@@ -0,0 +1,13 @@
+package lapnt.DuAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAn63132204ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
