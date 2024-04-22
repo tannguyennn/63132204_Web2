975febac30910380e204914ac5029971b6ee878e
NguyenTanLap <135099342+tannguyennn@users.noreply.github.com>
2024-04-22 15:03:09 +0700

    up sql moi

--- DuAn_63132204/src/main/java/lapnt/DuAn/Controllers/HomeController.java
+++ DuAn_63132204/src/main/java/lapnt/DuAn/Controllers/HomeController.java
@@ -1,8 +1,12 @@
 package lapnt.DuAn.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+	@GetMapping("/")
+	public String TrangChu() {
+		return "index";
+	}
 }
